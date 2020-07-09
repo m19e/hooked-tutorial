e69da181d53421d92c796f5095e4eb9b5f6a489b
m19e <nexus.me7290@gmail.com>
2020-07-10 00:08:45 +0900

    Display successfully

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const MOVIE_API_URL = "http://www.omdbapi.com/?s=paranormal&apikey=82b14814";
+
+const App = () => {
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    useEffect(() => {
+        fetch(MOVIE_API_URL)
+            .then((res) => res.json())
+            .then((jsonRes) => {
+                setMovies(jsonRes.Search);
+                setLoading(false);
+            });
+    }, []);
+
+    const search = (searchValue: string) => {
+        setLoading(true);
+        setErrorMessage(null);
+
+        fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=82b14814`)
+            .then((res) => res.json())
+            .then((jsonRes) => {
+                if (jsonRes.Response === "True") {
+                    setMovies(jsonRes.Search);
+                    setLoading(false);
+                } else {
+                    setErrorMessage(jsonRes.Error);
+                    setLoading(false);
+                }
+            });
+    };
+
+    return (
+        <div className="App">
+            <Header text="HOOKED" />
+            <Search search={search} />
+            <p className="movies">
+                {loading && !errorMessage ? (
+                    <span>loading...</span>
+                ) : errorMessage ? (
+                    <div className="errorMessage">{errorMessage}</div>
+                ) : (
+                    movies.map((movie: any, index) => (
+                        <Movie key={`${index}-${movie.Title}`} movie={movie} />
+                    ))
+                )}
+            </p>
+        </div>
+    );
+};
 
 export default App;
