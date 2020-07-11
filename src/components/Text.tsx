c9edea5a6d3289fc553a012c064c83ab6e70aa32
m19e <nexus.me7290@gmail.com>
2020-07-12 02:33:53 +0900

    Move vertical for ArrowUp/Down

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -10,6 +10,28 @@ const Text = (): JSX.Element => {
         if (e.key.includes("Arrow")) {
             e.preventDefault();
             console.log(`pressed ${e.key} key`);
+
+            const ta = e.target as HTMLTextAreaElement;
+            const start = ta.selectionStart;
+            const end = ta.selectionEnd;
+
+            console.log(`start: ${start}, end: ${end}`);
+
+            switch (e.key) {
+                case "ArrowDown":
+                    ta.setSelectionRange(start + 1, start + 1);
+                    break;
+                case "ArrowUp":
+                    if (start === 0) return;
+                    ta.setSelectionRange(start - 1, start - 1);
+                    break;
+                case "ArrowLeft":
+                    break;
+                case "ArrowRight":
+                    break;
+                default:
+                    break;
+            }
         }
     };
 
