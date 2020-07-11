a7ecc177d7fcbb8ba9dbc8e4a29bc374ee8d883b
m19e <nexus.me7290@gmail.com>
2020-07-11 23:44:33 +0900

    Use state for textarea value

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Text = () => {
-    const handleKeyDown = (e: any) => {
-        if (e.key === "Tab") {
+const Text = (): JSX.Element => {
+    const [text, setText] = useState("");
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setText(e.target.value);
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.key.includes("Arrow")) {
             e.preventDefault();
-            console.log(`pressed tab key`);
+            console.log(`pressed ${e.key} key`);
         }
     };
 
-    return <textarea onKeyDown={handleKeyDown}></textarea>;
+    return (
+        <textarea onChange={handleChange} onKeyDown={handleKeyDown}></textarea>
+    );
 };
 
 export default Text;
